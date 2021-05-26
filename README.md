611cc2285579192d72f463a6e690ce6ba401ba3b
sleepyboy <sleepyboy123@hotmail.com>
2021-05-26 22:04:17 +0800

    Initial Commit For SleepyMaps

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Sleepy Maps
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/component/Map/Map.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+import './Map.css';
+
+const Map = () => {
+    return(
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default Map;
